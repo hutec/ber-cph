07d9cbf8cbb6732c259b5f64043e6c878926bdfa
Robin Hutmacher <robin.hutmacher@gmail.com>
2022-09-18 21:33:47 +0200

    Add overview component (#3)
    * Update gitignore
    
    * Add overview component
    
    This component can show all routes at once in a different color
    
    * Add cycling color palette for overview map
    
    * Remove legacy Autopilot comments

--- components/map-preview.js
+++ components/map-preview.js
@@ -5,7 +5,9 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import path from "path";
 
-const Route = ({ track }) => {
+import chroma from "chroma-js";
+
+const Route = ({ track, updateMapBounds = true, color = "#000000" }) => {
   // See https://stackoverflow.com/questions/68758035/how-to-render-geojson-polygon-in-react-leaflet-mapcontainer
   const [geojson, setGeojson] = useState(0);
   const map = useMap();
@@ -14,24 +16,59 @@ const Route = ({ track }) => {
   useEffect(() => {
     const url = path.join(router.basePath, track);
 
-    // Replace .gpx with .geojson
     const geojsonFile = url.replace(".gpx", ".geojson");
 
-    // read geojson file
     fetch(geojsonFile)
       .then((response) => response.json())
       .then((data) => {
-        map.fitBounds(data.bounds, { padding: [50, 50] });
+        if (updateMapBounds) {
+          map.fitBounds(data.bounds, { padding: [50, 50] });
+        }
         setGeojson(data);
       });
   }, []);
 
   if (geojson) {
-    return <GeoJSON data={geojson} />;
+    return <GeoJSON data={geojson} color={color} />;
   } else {
     return null;
   }
 };
+
+export function MapOverview({ posts, bounds }) {
+  const tracks = posts.map((post) => post.track);
+
+  const colors = chroma.scale(["hotpink", "#2A4858"]).mode("lch").colors(3);
+  const tracksAndColors = tracks.map((track, index) => {
+    return { track, color: colors[index % colors.length] };
+  });
+
+  return (
+    <div>
+      <div className="my-5 h-1/2">
+        <MapContainer
+          bounds={bounds}
+          zoom={4}
+          scrollWheelZoom={true}
+          className="h-96 w-full"
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          {tracksAndColors.map((track) => (
+            <Route
+              key={track.track}
+              track={track.track}
+              updateMapBounds={false}
+              color={track.color}
+            />
+          ))}
+        </MapContainer>
+      </div>
+    </div>
+  );
+}
 
 export default function MapPreview({ track }) {
   return (
@@ -47,7 +84,7 @@ export default function MapPreview({ track }) {
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <Route track={track} />
+          <Route track={track} updateMapBounds={true} />
         </MapContainer>
       </div>
     </div>
